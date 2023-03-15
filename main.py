185eb968784ede182b415c132531576c13d597a5
outtasight1010 <126138474+outtasight1010@users.noreply.github.com>
2023-03-15 06:00:01 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from greeting import Greeting
+from gestures import Gestures
+
+
+
+games = Greeting()
+gesture = Gestures()
+
+games.open_greeting()
+games.game_rules()
+gesture.user_choice()
+
+
+
