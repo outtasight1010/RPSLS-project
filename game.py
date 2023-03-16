15c896427ca7a4951da52f10f5c66218ead8d68a
Michelle Lamelza <mishlamelza@gmail.com>
2023-03-16 14:08:52 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,8 +1,10 @@
 def main():
-
-
-
     gesture_list = ["Rock","Paper","Scissors","Lizard","Spock"]
+    player_choice = get_user_input()#this holds the choice in this function
+    second_choice = get_random_choice(gesture_list)# this holds the choice in this function
+    determine_winner(player_choice, second_choice, gesture_list)# determine winner function
+
+
 
 def get_user_input():# create function to get user(player 1) input
     
@@ -13,41 +15,103 @@ def get_user_input():# create function to get user(player 1) input
     else:
         return player_choice
     
-player_choice = get_user_input
 
-def get_random_choice():# create function to retrieve random gesture
-    gesture_list = ["Rock","Paper","Scissors","Lizard","Spock"]
+def get_random_choice(gesture_list):# create function to retrieve random gesture
+   
     import random
-    
     second_choice = random.choice(gesture_list)
+    
     return second_choice
 
-second_choice = get_random_choice
 
-def determine_winner():
-    player_choice = get_user_input
+def determine_winner(player_choice, second_choice, gesture_list):
     
-    second_choice = get_random_choice
-
-    import random
-    gesture_list = ["Rock","Paper","Scissors","Lizard","Spock"]
-    second_choice = random.choice(gesture_list)
     if player_choice == "1":
-        if second_choice == random.choice(gesture_list[1]):
+        if second_choice == (gesture_list[1]):
             print("You choose Rock.")
             print("AI chooses:",(gesture_list[1]))
             print("Paper covers Rock!")
             print("")
-    if player_choice == "1":
-        if second_choice == random.choice(gesture_list[2]): 
+        elif second_choice == (gesture_list[2]):
             print("You choose Rock.")
             print("AI chooses:",(gesture_list[2]))
             print("Rock crushes Scissors!")
+            print("")
+        elif second_choice == (gesture_list[3]):
+            print("You choose Rock.")
+            print("AI chooses:",(gesture_list[3]))
+            print("Rock crushes Lizard!")
+            print("")
+        elif second_choice == (gesture_list[4]):
+            print("You choose Rock.")
+            print("AI chooses:",(gesture_list[4]))
+            print("Spock vaporizes Rock!")
+            print("")
+        elif second_choice == (gesture_list[0]):
+            print("You choose Rock.")
+            print("AI chooses:",(gesture_list[0]))
+            print("TIE. Go again.")
+    elif player_choice == "2":
+        if second_choice == (gesture_list[0]):
+            print("You choose Paper.")
+            print("AI chooses:",(gesture_list[0]))
+            print("Paper covers Rock!")
+            print("")
+        elif second_choice == (gesture_list[1]):
+            print("You choose Paper.")
+            print("AI chooses:",(gesture_list[1]))
+            print("TIE. Go again.")
+        elif second_choice == (gesture_list[2]):
+            print("You choose Paper.")
+            print("AI chooses:",(gesture_list[2]))
+            print("Scissors cuts Paper!")
+            print("")
+        elif second_choice == (gesture_list[3]):
+            print("You choose Paper.")
+            print("AI chooses:",(gesture_list[3]))
+            print("Lizard eats Paper!")
+            print("")
+        elif second_choice == (gesture_list[4]):
+            print("You choose Paper.")
+            print("AI chooses:",(gesture_list[4]))
+            print("Paper disproves Spock!")
+            print("")
+    elif player_choice == "3":        
+        if second_choice == (gesture_list[0]):
+         
+
+         pass
+
+main()
 
 
-get_user_input()
-get_random_choice()
-determine_winner()
+
+
+
+
+
+
+
+
+        
+
+
+
+
+        
+
+
+        
+
+
+        
+
+
+        
+    
+
+
+
 
 
 
