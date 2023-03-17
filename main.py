e3151065e02e884f0735e90a85f283cb4abe7da2
Michelle Lamelza <mishlamelza@gmail.com>
2023-03-17 03:44:05 -0400

    added user story

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+# As a developer, I want to make at least 10 commits with descriptive messages.
+# As a developer, I want to find a way to properly incorporate inheritance into my game.
+# As a developer, I want to account for and handle bad user input, ensuring that any user input is validated and re-obtained if necessary.
+# As a developer, I want to store all of the gesture options/choices in a List (AS STRINGS). I want to find a way to utilize the list of gestures within my code (display gesture options, assign player a gesture, etc).
+# As a player, I want the correct player to win a given round based on the choices made by each player.  See below for game rules!
+# As a player, I want the game of RPSLS to be at minimum a “best of three” to decide a winner. 
+# As a player, I want the option of a single player (human vs AI) or a multiplayer (human vs human) game.
+
+
+
 import game
 
 
